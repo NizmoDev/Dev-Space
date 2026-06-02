9cef9bb5cb5bc4d8ec3eedddb99845690156442f
NizmoDev <denispodvin62120@gmail.com>
2026-06-02 15:07:53 +0200

    Dev-Space

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("DevSpace fonctionne 🚀");
+});
+
+app.listen(3000, () => {
+    console.log("Serveur lancé sur 3000");
+});
+
+console.log("PID:", process.pid);
+console.log("SERVER START");
